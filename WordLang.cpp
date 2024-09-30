cf60356ffea6fdead2d32c8a771ecb7891713cd6
Charles Ofria <charles.ofria@gmail.com>
2024-09-29 21:39:00 -0400

    Updated SymbolTable class.

--- WordLang.cpp
+++ WordLang.cpp
@@ -73,25 +73,49 @@ public:
 class SymbolTable {
 private:
   struct SymbolInfo {
+    std::string name;
     words_t words{};
     size_t declare_line;
 
-    SymbolInfo(size_t declare_line) : declare_line(declare_line) { }
+    SymbolInfo(std::string name, size_t declare_line)
+      : name(name), declare_line(declare_line) { }
   };
   std::vector<SymbolInfo> var_info;
   using scope_t = std::unordered_map<std::string, size_t>;
   std::vector<scope_t> scope_stack{1};
 
 public:
+  static constexpr size_t NO_ID = static_cast<size_t>(-1);
+
+  size_t GetVarID(std::string name) const {
+    for (auto it = scope_stack.rbegin();
+         it != scope_stack.rend();
+         ++it)
+    {
+      if (it->count(name)) return it->find(name)->second;
+    }
+
+    return NO_ID;
+  }
+
+  bool HasVar(std::string name) const {
+    return (GetVarID(name) != NO_ID);
+  }
+
   size_t AddVar(size_t line_num, std::string name) {
     auto & scope = scope_stack.back();
     if (scope.count(name)) {
       Error(line_num, "Redeclaring variable '", name, "'.");
     }
     size_t var_id = var_info.size();
-    var_info.emplace_back(line_num);
+    var_info.emplace_back(name, line_num);
     scope[name] = var_id;
     return var_id;
+  }
+
+  words_t & VarValue(size_t id) {
+    assert(id < var_info.size());
+    return var_info[id].words;
   }
 };
 
